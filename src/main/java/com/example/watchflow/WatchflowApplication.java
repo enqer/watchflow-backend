e7c902b34108f48bded1a6659cfb334f0caed948
enqer <pawel.kam4@gmail.com>
2023-12-24 16:17:27 +0100

    build: start project

---
+++ src/main/java/com/example/watchflow/WatchflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchflowApplication.class, args);
+	}
+
+}
